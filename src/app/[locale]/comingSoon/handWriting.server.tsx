74ccb5e882033adb99bd966902f658d94e543ce7
lillijo <lillijoppien@gmail.com>
2024-06-01 12:22:09 +0200

    fix dark mode handwriting

--- src/app/[locale]/comingSoon/handWriting.server.tsx
+++ src/app/[locale]/comingSoon/handWriting.server.tsx
@@ -2,12 +2,12 @@ import Image from 'next/image';
 
 export default async function ComingSoonHandWriting() {
   return (
-      <Image
-        src="/writing.png"
-        width={1207}
-        height={587}
-        alt="Hand writing"
-        className="h-auto max-w-[calc(100vw-4rem)] min-w-[calc(60vw-4rem)]"
-      />
+    <Image
+      src="/writing.png"
+      width={1207}
+      height={587}
+      alt="Hand writing"
+      className="h-auto min-w-[calc(60vw-4rem)] max-w-[calc(100vw-4rem)] dark:invert"
+    />
   );
 }
