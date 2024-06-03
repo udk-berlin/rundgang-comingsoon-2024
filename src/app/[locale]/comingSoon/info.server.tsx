045120fa5b80846fc27fe014d2cfb5d08bbc2f7c
lillijo <lillijoppien@gmail.com>
2024-06-03 15:34:41 +0200

    add target blank

--- src/app/[locale]/comingSoon/info.server.tsx
+++ src/app/[locale]/comingSoon/info.server.tsx
@@ -3,10 +3,24 @@ import { getMessages } from '@/config';
 export default async function ComingSoonInfo({ locale }: { locale: string }) {
   const t = await getMessages(locale);
   return (
-    <div className="h-[8rem] flex flex-col items-center justify-center text-center">
+    <div className="flex h-[8rem] flex-col items-center justify-center text-center">
       <p>{t.text}</p>
-      <p>&rarr; <a className="hover:underline" href={t.generalInformation.href}>{t.generalInformation.label}</a></p>
-      <p>&rarr; <a className="hover:underline" href={t.previous.href}>{t.previous.label}</a></p>
+      <p>
+        &rarr;{' '}
+        <a
+          target="_blank"
+          className="hover:underline"
+          href={t.generalInformation.href}
+        >
+          {t.generalInformation.label}
+        </a>
+      </p>
+      <p>
+        &rarr;{' '}
+        <a target="_blank" className="hover:underline" href={t.previous.href}>
+          {t.previous.label}
+        </a>
+      </p>
     </div>
   );
 }
--- src/app/[locale]/footer.tsx
+++ src/app/[locale]/footer.tsx
@@ -3,12 +3,16 @@ import { getMessages } from '@/config';
 export default async function Footer({ locale }: { locale: string }) {
   const t = await getMessages(locale);
   return (
-    <footer className="mx-[2rem] h-[5rem] flex items-center justify-between">
+    <footer className="mx-[2rem] flex h-[5rem] items-center justify-between">
       <div>
-        <a href={t.contact.href}>{t.contact.label}</a>
+        <a target="_blank" href={t.contact.href}>
+          {t.contact.label}
+        </a>
       </div>
       <div>
-        <a href={t.imprint.href}>{t.imprint.label}</a>
+        <a target="_blank" href={t.imprint.href}>
+          {t.imprint.label}
+        </a>
       </div>
     </footer>
   );
