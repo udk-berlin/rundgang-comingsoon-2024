9ed9b07bcfde340dae3e5ee5746e86b0390e11ee
marisanest <marisanest@mailbox.org>
2024-06-06 10:34:54 +0200

    Add more hand writings.

--- src/app/[locale]/comingSoon/handWriting.server.tsx
+++ src/app/[locale]/comingSoon/handWriting.server.tsx
@@ -2,31 +2,32 @@
 import { useEffect, useState } from 'react';
 import Image from 'next/image';
 
+const imageIds = [1, 2, 3, 4, 5, 6, 7, 8];
+
 export default function ComingSoonHandWriting() {
-  const [currentIndex, setCurrentIndex] = useState(1);
+  const [currentImageId, setCurrentImageId] = useState(1);
 
   useEffect(() => {
     const timer = setInterval(() => {
-      if (currentIndex === 8) {
-        setCurrentIndex(1);
-      } else {
-        setCurrentIndex(currentIndex + 1);
-      }
-    }, 5000); // clearing interval
+      const nextImageId =
+        currentImageId === imageIds.length ? 1 : currentImageId + 1;
+      setCurrentImageId(nextImageId);
+    }, 5000);
+
     return () => clearInterval(timer);
   });
 
   return (
     <div className="relative">
-      {[1, 2, 3, 4, 5, 6, 7, 8].map((number) => (
+      {imageIds.map((imageId) => (
         <div
-          key={`handwriting-image-${number}`}
+          key={imageId}
           className="pointer-events-none fixed bottom-0 left-0 right-0 top-0 flex items-center justify-center transition-[opacity] duration-[2.5s] ease-in-out"
-          style={{ opacity: currentIndex === number ? 1 : 0 }}
+          style={{ opacity: currentImageId === imageId ? 1 : 0 }}
         >
           <Image
-            key={`handwriting-image-${number}`}
-            src={`/${number}-writing.png`}
+            key={imageId}
+            src={`/${imageId}-writing.png`}
             width={1207}
             height={587}
             alt="Hand writing"
